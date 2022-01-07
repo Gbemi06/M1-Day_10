76f01bd7dc8d0be5cb49ba2be805363f3acea414
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2022-01-07 13:03:05 +0200

    JS Exercise updated

--- index.js
+++ index.js
@@ -34,13 +34,74 @@ console.log(createArray)
 for (let i = 0; i < createArray.length; i++) {
     console.log(createArray[i])
 }
-        
-        
-        
-       
+
+//26) Create an array with 100 random numbers in it
+let randomNumber = []
+for (let i = 0; i < 100; i++) {
+    randomNumber.push(Math.floor(Math.random() * 100))
+}
+console.log(randomNumber)
+
+//27) Write a function to get the maximum and minimum values from the previously created array
+let minAndMax = function() {
+    let min = randomNumber[0]
+    let max = randomNumber[0]
+    for (let i=0; i < randomNumber.length; i++){
+        let currentNumber = randomNumber[i]
+        if (currentNumber < min){
+            min = currentNumber
+        }
+        if (currentNumber > max){
+            max = currentNumber
+        }
+    }
+    console.log(max)
+    console.log(min)
+}
+
+minAndMax()
+
+// 28) Create an array of arrays, in which every array has 10 random numbers
+
+let createArraysOfArray = function(){
+    let outerArray = []
+    for (let i = 0; i < 10; i++){
+        innerArray = []
+        for (let j = 0; j < 10; j++) {
+            innerArray.push(Math.floor(Math.random() * 10))
+        }
+        outerArray.push(innerArray)
+    }
+    console.log(outerArray)
+}
+
+createArraysOfArray()
+
+
+// 31) Get the element with an id of "container" from the page
+
+//const list = document.querySelectorAll("#container")
+//console.log(list)
+
+const led = document.querySelectorAll("h1")[0]
+console.log(led)
+
+
         /*
-        26) Create an array with 100 random numbers in it
-        27) Write a function to get the maximum and minimum values from the previously created array
-        28) Create an array of arrays, in which every array has 10 random numbers
+        
         29) Create a function that gets 2 arrays as parameters and returns the longest one
-        30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values */
+        30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values 
+        
+         DOM EXERCISES
+
+    
+        32) Get every <td> element from the page
+        33) Use a loop for printing the text inside of every <td> element in the page
+        34) Write a function to change the heading of the page
+        35) Write a function to add an extra row to the table
+        36) Write a function to add a class of "test" to each row in the table
+        37) Write a function to add a red background to every link in the page
+        38) Console log "Page loaded" when the page is correctly loaded
+        39) Write a function to add new items to a unordered list
+        40) Write a function to empty a list
+        */
