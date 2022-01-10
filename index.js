8c92d530d45fcd6bc0c871da2806e8abde91e849
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2022-01-10 10:05:57 +0200

    JS Exercise Update

--- index.js
+++ index.js
@@ -121,13 +121,58 @@ const list = document.getElementById("container")
 let tableData = document.querySelectorAll("td")
 
 // 33) Use a loop for printing the text inside of every <td> element in the page
-for (i = 0; i < tableData.length; i++) {
+/*for (i = 0; i < tableData.length; i++) {
     tableData[i].innerText = "Text"
-}
+}*/
 
 // 34) Write a function to change the heading of the page
+let changeHeading = function() {
+    let h1Heading = document.querySelector("h1")
+    h1Heading.innerText = "Change Title Head"
+}
 
+changeHeading()
 
+// 35) Write a function to add an extra row to the table
+const createNewCell = function() {
+    let newTable = document.querySelector("tbody")
+    let tr = document.createElement("tr")
+    for (i = 0; i < 5; i++) {
+        let td = document.createElement("td")
+        td.innerText = "cell " + (i+1)
+        tr.appendChild(td)
+    }
+    newTable.appendChild(tr)
+}
+createNewCell()
+
+// 36) Write a function to add a class of "test" to each row in the table
+
+const addNewClass = function() {
+    let trs = document.querySelectorAll("tr")
+    for (i = 0; i < trs.length; i++) {
+        trs[i].classList.add('test')
+    }
+    
+}
+addNewClass()
+
+// 37) Write a function to add a red background to every link in the page
+
+const addColor = function() {
+    let aLinks = document.querySelectorAll("a")
+    for (i = 0; i < aLinks.length; i++){
+        aLinks[i].classList.add('aTag-color')
+    }
+}
+
+addColor()
+
+//38) Console log "Page loaded" when the page is correctly loaded
+
+window.onload = function(){
+    console.log('Page loaded')
+}
 
         /*
         
@@ -139,10 +184,10 @@ for (i = 0; i < tableData.length; i++) {
         
        
         
-        35) Write a function to add an extra row to the table
-        36) Write a function to add a class of "test" to each row in the table
-        37) Write a function to add a red background to every link in the page
-        38) Console log "Page loaded" when the page is correctly loaded
+        
+        
+        
+        
         39) Write a function to add new items to a unordered list
         40) Write a function to empty a list
         */
